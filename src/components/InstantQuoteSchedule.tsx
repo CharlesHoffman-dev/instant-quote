6924d05c5a39df4a7d431867b0f6ea1d30d3e6d9
Charles Hoffman <charlesirahoffman@gmail.com>
2025-10-25 00:10:18 -0500

    Bug fixes

--- src/components/InstantQuoteSchedule.tsx
+++ src/components/InstantQuoteSchedule.tsx
@@ -317,7 +317,7 @@ export default function InstantQuoteSchedule() {
                 disabled={!canSchedule}
                 onClick={() => {
                   if (!canSchedule) return;
-                  window.location.href = bookingUrl;
+                  window.open(bookingUrl, "_blank", "noopener,noreferrer"); // <-- back to new tab
                 }}
               >
                 <Calendar className="w-4 h-4 mr-2" /> Schedule Now
